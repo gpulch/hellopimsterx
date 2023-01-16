d054c96debabbb3bee26e715accdff6a520c0534
gpulch <42658765+gpulch@users.noreply.github.com>
2023-01-16 17:42:50 +0100

    trying a fix for vercel deployment

--- components/head/bodyComponent.tsx
+++ components/head/bodyComponent.tsx
@@ -9,16 +9,17 @@ import {ILaunch} from "../../pages/index";
 export interface BodyComponentProps {
   missionName: string;
   launchesPast: any;
+  onSearch: (query: string) => void;
 }
 
-const bodyComponent = ({ missionName, launchesPast }: BodyComponentProps) => {
+const bodyComponent = ({ onSearch, launchesPast }: BodyComponentProps) => {
   return (
     <div className={styles.container}>
       {/* {JSON.stringify(data)} */}
       <HomePageHead />
       <div className="topSection">
         <img src={logo.src} alt="logo" className="logo" />
-        <SearchBar missionName={missionName} />
+        <SearchBar onSearch={onSearch}/>
       </div>
       <LaunchCards launchesPast={launchesPast} />
     </div>
